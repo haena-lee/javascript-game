735edb00e026e8a202dacfc6a668182a66d68128
Haena Lee <A01074200@ad.bcit.ca>
2018-06-26 15:53:43 -0700

    Added row and column divs

---
+++ scripts/script.js
@@ -0,0 +1,46 @@
+const cardsArray = [
+    {
+        'name': 'beagle',
+        'image': '../images/beagle.png'
+    },
+    {
+        'name': 'chihuahua',
+        'image': '../images/chihuahua.png'
+    },
+    {
+        'name': 'french-bulldog',
+        'image': '../images/french-bulldog.png'
+    },
+    {
+        'name': 'golden-lab',
+        'image': '../images/golden-lab.png'
+    },
+    {
+        'name': 'grandpa-dog',
+        'image': '../images/grandpa-dog.png'
+    },
+    {
+        'name': 'husky',
+        'image': '../images/jindo.png'
+    },
+    {
+        'name': 'jack-russell-terrier',
+        'image': '../images/jack-russell-terrier.png'
+    },
+    {
+        'name': 'jindo',
+        'image': '../images/jindo.png'
+    },
+    {
+        'name': 'pug',
+        'image': '../images/pug.png'
+    },
+    {
+        'name': 'shaggy-dog',
+        'image': '../images/shaggy-dog.png'
+    },
+    {
+        'name': 'shiba-inu',
+        'image': '../images/shiba-inu.png'
+    }
+]
