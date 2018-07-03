915b2083a95e232a808284faf2ba54a33aca5095
haena <32079807+haenabee@users.noreply.github.com>
2018-07-02 17:48:53 -0700

    added guess and match counters

--- scripts/script.js
+++ scripts/script.js
@@ -62,6 +62,9 @@ let count = 0;
 let previousTarget = null;
 let delay = 1200;
 
+var matchCount = 0;
+var clickCount = 0;
+
 grid.addEventListener('click', function(e){
     let clicked = e.target;
     if (clicked.nodeName === 'SECTION' || clicked === previousTarget || clicked.parentNode.classList.contains('selected')) {return;}
@@ -78,9 +81,17 @@ grid.addEventListener('click', function(e){
             clicked.parentNode.classList.add('selected');
         }
         if (firstGuess !== '' && secondGuess !== '') {
+            clickCount++;
+            document.getElementById("clickCount").innerHTML = clickCount;
             if (firstGuess === secondGuess) {
                 setTimeout(match, delay);
                 setTimeout(resetGuesses, delay);
+                matchCount++;
+                document.getElementById("matches").innerHTML = matchCount;
+                console.log(`Number of matches: ${matchCount}`);
+                if (matchCount == (gameGrid.length / 2)) {
+                    document.getElementById("matches").innerHTML = "All the puppies found their moms!";
+                }
             } else {
                 setTimeout(resetGuesses, delay);
             }
@@ -110,5 +121,16 @@ const resetGuesses = () => {
         card.classList.remove('selected');
     });
 }
+
+function resetGame() {
+    matchCount = 0;
+    document.getElementById("matches").innerHTML = matchCount;
+    clickCount = 0;
+    document.getElementById("clickCount").innerHTML = clickCount;
+    var divClasses = document.querySelectorAll('.match');
+    divClasses.classList.remove('match');
+    gameGrid.sort(() => 0.5 - Math.random());
+}
+
 
 
